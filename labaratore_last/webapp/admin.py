242ced6d57402d0ea0b87d99591db311bb8e42e3
Bars <barsbekclash@gmail.com>
2021-05-28 13:38:44 +0600

    add Models Quote and register in Admin

--- labaratore_last/webapp/admin.py
+++ labaratore_last/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from webapp.models import Quote
 # Register your models here.
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'email', 'rating', 'date']
+    list_filter = ['name', 'rating']
+    search_fields = ['name', 'text']
+    fields = ['id', 'text', 'name', 'email', 'rating', 'status', 'date']
+    readonly_fields = ['date', 'id',]
+
+
+admin.site.register(Quote, QuoteAdmin)
--- labaratore_last/webapp/models.py
+++ labaratore_last/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
 # Create your models here.
+choices = [('new', 'New'), ('moder', 'Moder')]
+
+class Quote(models.Model):
+    text = models.CharField(max_length=3000, null=False, blank=False)
+    name = models.CharField(max_length=300, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    rating = models.IntegerField(null=False, blank=False, default=0)
+    status = models.CharField(max_length=300, null=False, blank=False, choices=choices, default='new')
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'Quote'
+        verbose_name = 'Цитата'
+        verbose_name_plural = 'Цитаты'
+
+    def __str__(self):
+        return f"{self.name} - {self.date} : {self.rating}"
